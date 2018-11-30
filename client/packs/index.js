e1ff91cbc759c1cd8c5ca7c783db88d7f60f6b42
Pelumi Longe <longe.pelumi@yahoo.com>
2018-11-30 05:40:43 +0100

    setup frontend routing with react

--- client/packs/index.js
+++ client/packs/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 import App from '../components/App';
+
+// This is to be used when sending requests to the rails backend
+const csrfToken = document.querySelector('[name=csrf-token]').content;
+axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken;
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App />,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
     document.body.appendChild(document.createElement('div')),
   );
 });
