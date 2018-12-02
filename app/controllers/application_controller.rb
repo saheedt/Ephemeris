e6a2c5149fa11f2b6c26351c2c5964000b22730e
Saheed Ajibulu <stunnafrsh007@gmail.com>
2018-12-02 17:46:14 +0100

    - add rubocop config for linting ruby / rails files.
    [Finishes #162335484]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  prepend_view_path Rails.root.join('client')
+  prepend_view_path Rails.root.join("client")
 end
