08611df82fe6c034adc12227d9bc533afc169899
saheedt <stunnafrsh007@gmail.com>
2018-12-13 12:59:13 +0100

    - add graphql and install all dependencies (#6)
    [Finishes #162416101]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
   root to: 'home#index'
 
   # This should be the last route to be defined as it is meant to
