30fa1ce88ec9808d9d4cbe613c4acedc5eaec891
Saheed Ajibulu <stunnafrsh007@gmail.com>
2019-11-07 15:56:44 +0100

    [HOTFIX]: Another supposed fix for CSRF token authenticity by forcing rails to prepend to before_action chsin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery prepend: true, with: :exception
+  protect_from_forgery prepend: true
   prepend_view_path Rails.root.join("client")
 end
