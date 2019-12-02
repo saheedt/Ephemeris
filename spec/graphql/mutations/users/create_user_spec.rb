e00818d44cdea679ea33225569cc188aef62b1b9
Saheed Ajibulu <stunnafrsh007@gmail.com>
2019-12-03 00:09:41 +0100

    - add userLogin mutation test - update createUser mutation test
    [Finishes #162416628]

--- spec/graphql/mutations/users/create_user_spec.rb
+++ spec/graphql/mutations/users/create_user_spec.rb
@@ -4,7 +4,11 @@ module Mutations
   module Users
     RSpec.describe CreateUser, type: :request do
       describe '.reslove' do
-        it 'creates a user' do
+        after(:all) do
+          User.destroy_all
+        end
+
+        it 'should creates a user' do
           user = { email: "a@b.com", screen_name: "tester_a", password: "testingtester",
                    password_confirmation: "testingtester", name: "Test A"}
 
@@ -23,7 +27,24 @@ module Mutations
                             'errors' => nil
                           )
         end
+
+        it 'should not creates a user with incomplete credentials' do
+          user = { email: "a@b.com", screen_name: "tester_a", password: "testingtester",
+                   password_confirmation: "testing", name: "Test A"}
+
+          post '/graphql', params: { query: graphQuery(user) }
+
+          json = JSON.parse(response.body)
+          data = json['data']['createUser']
+
+          expect(data).to include(
+                            'user' => nil,
+                            'token' => nil,
+                            'errors' => [ "Password confirmation doesn't match Password" ]
+                          )
+        end
       end
+      
       def graphQuery(user)
         <<~GQL
           mutation {
