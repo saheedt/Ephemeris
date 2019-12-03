78884cebbbb32dbaa628e4dfe2e504bbec0a5525
Saheed Ajibulu <stunnafrsh007@gmail.com>
2019-12-03 16:51:16 +0100

    - Change user migration to activate 'pgcrypto'extension for UUID - Change user migration to add field for UUID + extra data validation on DB level - Modify GraphQL user type to account for UUID

--- db/migrate/20191104114118_create_users.rb
+++ db/migrate/20191104114118_create_users.rb
@@ -1,10 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
+    enable_extension 'pgcrypto'
+
     create_table :users do |t|
-      t.string :screen_name, index: {unique: true}
+      t.string :screen_name, null: false, index: { unique: true }
       t.string :name
-      t.string :email, index: {unique: true}
-      t.string :password_digest
+      t.string :email, null: false, index: { unique: true }
+      t.string :password_digest, null: false
+      t.uuid :uuid, null: false, index: { unique: true }
 
       t.timestamps
     end
